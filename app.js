96d15badb39e68bae70efafaeb23dd19b06adc8f
Arthur Sakemi <arthursakemi@gmail.com>
2020-04-17 14:04:16 -0300

    setUser and getUser functions created

--- app.js
+++ app.js
@@ -3,20 +3,32 @@ require("dotenv").config();
 const Telegraf = require("telegraf");
 const menus = require("./src/menus/menus");
 
-const codeStats = require("./src/codeStats");
-
-const bot = new Telegraf(process.env.BOT_API_KEY);
-
 const {
     getUserStats
-} = codeStats;
+} = require("./src/codeStats");
+
+const bot = new Telegraf(process.env.BOT_API_KEY);
 
 const helpText = `
 
 `;
 
+let userNames = new Map();
+
 bot.start((ctx) => ctx.reply("Welcome mate!"));
 bot.help((ctx) => ctx.reply("Not yet fella!"));
+
+bot.hears(/\/setUser (.+)/, (ctx) => {
+    const userName = ctx.from.username;
+    const newUserName = ctx.match[1];
+
+    userNames.set(userName, newUserName);
+
+    ctx.reply(`@${userName}'s username set to ${newUserName}`);
+
+    console.log(userNames);
+
+});
 
 bot.command("stats", (ctx) => {
     const user = ctx.from.username;
@@ -24,10 +36,28 @@ bot.command("stats", (ctx) => {
     ctx.reply(`Tell me watcha need @${user}!`, menus.mainMenu);
 });
 
-bot.command("userTotalXP", (ctx) => {
-    const userName = ctx.from.username
+bot.command('getUser', (ctx) => {
+    const userName = ctx.from.username;
 
-    const xp = getUserStats(userName)
+    if (!userNames.has(userName)) {
+        userNames.set(userName, userName)
+    }
+
+    const codeStatsUser = userNames.get(userName);
+
+    ctx.reply(`@${userName}, your CodeStats username is: ${codeStatsUser}`);
+});
+
+bot.command("userTotalXP", (ctx) => {
+    const userName = ctx.from.username;
+
+    if (!userNames.has(userName)) {
+        userNames.set(userName, userName)
+    }
+
+    const codeStatsUser = userNames.get(userName);
+
+    getUserStats(codeStatsUser)
         .then((response) => {
             ctx.reply(`@${userName} your total XP is: ${response.data.total_xp}`);
         });
