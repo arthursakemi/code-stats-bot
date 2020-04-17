d97ec39a8106d5ef66538cc9d7678656fb2a7628
Arthur Sakemi <arthursakemi@gmail.com>
2020-04-17 10:30:15 -0300

    initial commands set

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 require("dotenv").config();
+
+const Telegraf = require("telegraf");
+
+const bot = new Telegraf(process.env.BOT_API_KEY);
+
+const helpText = `
+
+`;
+
+bot.start((ctx) => ctx.reply("Welcome mate!"));
+bot.help((ctx) => ctx.reply("Not yet fella!"));
+
+bot.command("stats", (ctx) => {
+    const user = ctx.user.id;
+    ctx.reply(`Have some stats ${user}!`);
+})
+
+
+bot.launch();
